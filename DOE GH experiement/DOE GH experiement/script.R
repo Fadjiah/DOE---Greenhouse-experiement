0893363f9ddc2d329866673d09af277d2355c7c8
ewenharrison <fadjiahcollin@gmail.com>
2017-02-23 18:12:46 -0500

    upload data

---
+++ DOE GH experiement/DOE GH experiement/script.R
@@ -0,0 +1,6 @@
+# import data
+greenHouseDOE_df <- read.csv(file = "C:/Users/fadji_000/AppData/Local/Temp/Prejet serre DOE.csv.utf8", header = TRUE, row.names = NULL, encoding = "UTF-8", sep = ";", dec = ".", quote = "\"", comment.char = "")
+
+# convert natural variable to code variable
+
+if
