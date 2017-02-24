aeb396e10e12dc5947cb385cbab704922148e9fc
ewenharrison <fadjiahcollin@gmail.com>
2017-02-23 23:06:12 -0500

    Code segmentation

--- DOE GH experiement/DOE GH experiement/script.R
+++ DOE GH experiement/DOE GH experiement/script.R
@@ -1,19 +1,41 @@
+##### LOAD INFORMATION #####
 # pakage 
 library(ggplot2)
+library(ggcorrplot)
 # import data
 doe_df <- read.csv(file = "C:/Users/fadji_000/AppData/Local/Temp/Projet serre DOE.csv.utf8", header = TRUE, row.names = NULL, encoding = "UTF-8", sep = ";", dec = ",", quote = "\"", comment.char = "")
 
+#### DATA CLEANNING #############
+
+#verify 
+print(doe_df) 
+
 # Verify the class of each column
-lapply(doe_df,class)
+sapply(doe_df, class)
+
+# change class of column
+doe_df$serie <- as.factor(doe_df$serie)
+doe_df$position <- as.factor(doe_df$position)
+doe_df$bloc <- as.factor(doe_df$bloc)
+
+# Change column name result to height
+names(doe_df)[names(doe_df) == 'result'] <- 'height'
+
+#lowercase name Bloc
+names(doe_df)[names(doe_df) == 'Bloc'] <- 'bloc'
+
+
+#verify 
+print(names(doe_df[8]))
 
 
 # Convert to NA all 
-for (i in 1:length(doe_df$result)) {
-   if (doe_df$result[i]==0) {
-      doe_df$result[i]<-NA
+for (i in 1:length(doe_df$height)) {
+   if (doe_df$height[i]==0) {
+      doe_df$height[i]<-NA
      }
 }
-
+#verify
 print(doe_df)
 
 # convert natural variable to coded variable
@@ -34,17 +56,20 @@ levels(doe_df$light)[2] <- 1
 catnipDoe_df <- subset(doe_df, plant == "Catnip")
 fennelDoe_df <- subset(doe_df, plant == "Fennel")
 
+####### ANALISYS #############
+
 #apply anova to each subset
 #catnip
-lm(doe_df$result)
-
-catnipDoe_aov2 <- anova(lm(result) ~ light*water*fertilizer, data = catnipDoe_df)
+catnipDoe_aov2 <- anova(lm(as.double(height) ~ light * water * fertilizer, data = catnipDoe_df))
 print(catnipDoe_aov2)
 
 #Fennel
-fennelDoe_df <- anova(lm(as.double(result) ~ light * water * fertilizer, data = fennelDoe_df))
+fennelDoe_df <- anova(lm(as.double(height) ~ light * water * fertilizer, data = fennelDoe_df))
 print(fennelDoe_df)
 
+####### PLOT #############
 
-ggplot(doe_df, aes(x = id, y = as.double(result), col = plant)) + geom_point()+geom_smooth()
+ggplot(doe_df, aes(x = id, y = as.double(height), col = plant)) + geom_point(aes(shape = bloc)) + geom_smooth(na.rm = TRUE)
+
+ggplot(doe_df, aes(x = id, y = as.double(height), col = plant)) + geom_boxplot()
 
